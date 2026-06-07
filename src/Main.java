868a3b9771a4927e0e5fa8e92ac49ea317ea91b2
Hamza <hamzamayou04@gmail.com>
2026-06-07 12:19:29 +0100

    proper getlineage function and fixed bug of infinite loop

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Node ful1 = new Node(1, "fulan");
+        Node ful2 = new Node(2, "flan");
+        Node ful3 = new Node(3, "filan");
+        
+        ful2.setFather(ful1);  
+        ful3.setFather(ful2);
+
+        System.out.println(ful2.getFather().getName());
+        System.out.println(ful3.nassab());
+        System.out.println(ful3.getLineage());
     }
 }
--- src/Node.java
+++ src/Node.java
@@ -1,42 +1,80 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Node {
+    int id;
+    String name = "Fulan";
+    String gender;
+    Node father = null;
+    Node mother = null;
 
-    static String name;
-    static Node father;
-    static Node mother;
-
-    public Node() {
-
+    public Node(int pId, String pName) {
+        id = pId;
+        name = pName;
+        // gender = pGender;
     }
 
+    // getters and setters
+    public int getId () {
+        return this.id;
+    }
+    public void setId (int id) {
+        this.id = id;
+    }
     public String getName () {
-        return Node.name;
+        return this.name;
     }
-
     public void setName (String name) {
-        Node.name = name;
+        this.name = name;
     }
-
+    // public String getGender () {
+    //     return Node.gender;
+    // }
+    // public void setGender (String gender) {
+    //     Node.gender = gender;
+    // }
     public Node getFather () {
-        return Node.father;
+        return this.father;
     }
-
     public void setFather (Node father) {
-        Node.father = father;
+        this.father = father;
     }
-
     public Node getMother () {
-        return Node.mother;
+        return this.mother;
     }
-
     public void setMother (Node mother) {
-        Node.mother = mother;
+        this.mother = mother;
     }
-
     public void removeFather () {
-        Node.father = null;
+        this.father = null;
+    }
+    public void removeMother () {
+        this.mother = null;
     }
 
-    public void removeMother () {
-        Node.mother = null;
+    // others
+    public String nassab () {
+        Node current = this;
+        String nassab = current.getName();
+        while (current.getFather() != null) {
+            current = current.getFather();
+            nassab = nassab + " b. " +  current.getName();
+        }
+        return nassab;
     }
+
+    public List<Node> getLineage () {
+        Node current = this;
+        List<Node> lineage = new ArrayList<>();
+        while (current.getFather() != null) {
+            current = current.getFather();
+            lineage.add(current);
+        }
+        return lineage;
+    }
+    
+
+    // public String descendants () {
+        
+    // }
 }
